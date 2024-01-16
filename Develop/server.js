268925d1a54ba172381dd8d7027641182a45a93f
RockyWearsAHat <alexwaldmann2004@gmail.com>
2024-01-15 20:27:59 -0700

    Note-Taker

---
+++ Develop/server.js
@@ -0,0 +1,86 @@
+import express from "express";
+import fs from "fs";
+
+const port = 3000;
+const app = new express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(process.cwd() + "/public"));
+
+app.get("/api/notes", (req, res) => {
+  let fileContents = JSON.parse(
+    fs.readFileSync(process.cwd() + "/db/db.json").toString()
+  );
+  res.json(fileContents);
+});
+
+app.post(
+  "/api/notes",
+  (req, res, next) => {
+    if (!req.body.title || !req.body.text) {
+      res
+        .status(500)
+        .json("Error posting note, title and body for note must not be blank");
+
+      return;
+    }
+    next();
+  },
+  (req, res) => {
+    let fileContents = JSON.parse(
+      fs.readFileSync(process.cwd() + "/db/db.json").toString()
+    );
+    fileContents.push({
+      id:
+        fileContents.length > 0
+          ? fileContents[fileContents.length - 1].id + 1
+          : 0,
+      title: req.body.title,
+      text: req.body.text,
+    });
+    fs.writeFileSync(
+      process.cwd() + "/db/db.json",
+      JSON.stringify(fileContents, null, 2)
+    );
+    res.json(fileContents);
+  }
+);
+
+app.delete("/api/notes/:id", (req, res) => {
+  req.params.id;
+
+  let fileContents = JSON.parse(
+    fs.readFileSync(process.cwd() + "/db/db.json").toString()
+  );
+
+  for (let i = 0; i < fileContents.length; i++) {
+    if (req.params.id == fileContents[i].id) {
+      fileContents.splice(i, 1);
+    }
+  }
+
+  fs.writeFileSync(
+    process.cwd() + "/db/db.json",
+    JSON.stringify(fileContents, null, 2)
+  );
+
+  res.json(fileContents);
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile("/notes.html", { root: "public" });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile("/index.html", { root: "public" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile("/index.html", { root: "public" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
